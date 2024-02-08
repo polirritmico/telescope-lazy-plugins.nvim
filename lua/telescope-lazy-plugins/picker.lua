af18657d421002fe479b086f912473cb5cb1fd40
polirritmico <ebray187@gmail.com>
2024-02-07 22:19:24 -0300

    refactor: Clean helper functions

--- lua/telescope-lazy-plugins/picker.lua
+++ lua/telescope-lazy-plugins/picker.lua
@@ -2,65 +2,70 @@ local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local config = require("telescope.config").values
 
----@class TelescopeLazyPicker Telescope picker to select plugins from the Lazy spec
+---Telescope picker to select plugins from the Lazy spec
+---@class TelescopeLazyPicker
 local TelescopeLazyPicker = {}
 
--- TODO: plugin spec data: name, git name, filepath, linenumber
-function TelescopeLazyPicker._get_plugins_data()
+---Stores the relevant Lazy plugin spec data to use the picker.
+---@class LazyPluginData
+---@field name string Plugin name
+---@field repo_name string Full name of the plugin repository
+---@field filepath string Full file path to the plugin lua configuration
+---@field line integer Line number of the plugin definition in the lua file
+local LazyPluginSpecData = {}
+
+---Finds the line number matching the plugin repository name within the plugin file and updates the 'line' field of the provided 'plugin' object accordingly.
+---@param plugin LazyPluginData
+local function _find_line_number(plugin)
+  local line = 1
+  for line_str in io.lines(plugin.filepath) do
+    if string.find(line_str, plugin.repo_name, 1, true) then
+      plugin.line = line
+      return
+    end
+    line = line + 1
+  end
+end
+
+---Get the Lazy plugin data from the Lazy specification. For each plugin,
+---obtains the plugin name, repository name (<username/plugin>), full file path
+---of the Lua file containing the plugin config, and the line number where the
+---repository name is found.
+---@return table<LazyPluginData>
+local function _get_plugins_data()
   local spec_files = {}
   local lazy_plugins = require("lazy").plugins()
   local config_path = vim.fn.stdpath("config")
 
-  for _, plugin in pairs(lazy_plugins) do
-    local name = plugin.name
-    local repo_name = plugin[1]
-    if name ~= "lazy.nvim" then
-      local plugin_module = ""
-      if plugin._.super ~= nil then
-        plugin_module = plugin._.super._.module:gsub("%.", "/")
-      else
-        plugin_module = plugin._.module:gsub("%.", "/")
-      end
-      local module_path = string.format("%s/lua/%s.lua", config_path, plugin_module)
-      table.insert(spec_files, { name, repo_name, module_path, 0 })
+  for _, lazy_plugin in pairs(lazy_plugins) do
+    ---@type LazyPluginData
+    local plugin = { name = "", repo_name = "", filepath = "", line = 0 }
+    plugin.name = lazy_plugin.name
+    plugin.repo_name = lazy_plugin[1]
+    if plugin.name ~= "lazy.nvim" then
+      local module = lazy_plugin._.super == nil and lazy_plugin._.module
+        or lazy_plugin._.super._.module
+      module = module:gsub("%.", "/")
+      plugin.filepath = string.format("%s/lua/%s.lua", config_path, module)
+
+      _find_line_number(plugin)
+      table.insert(spec_files, plugin)
     end
   end
   return spec_files
 end
 
-function TelescopeLazyPicker.index_plugins_data(data)
-  local function find_line_number(search_string, filename)
-    local line_number = 1
-    for line in io.lines(filename) do
-      if string.find(line, search_string, 1, true) then
-        return line_number
-      end
-      line_number = line_number + 1
-    end
-    return 1
-  end
-
-  for _, plugin in pairs(data) do
-    if type(plugin) ~= "number" then
-      local name = plugin[2]
-      local path = plugin[3]
-      plugin[4] = find_line_number(name, path)
-    end
-  end
-  return data
-end
-
 function TelescopeLazyPicker.finder()
-  local plugins_data = TelescopeLazyPicker._get_plugins_data()
   local plugins = {
-    results = TelescopeLazyPicker.index_plugins_data(plugins_data),
+    results = _get_plugins_data(),
+    ---@param entry LazyPluginData
     entry_maker = function(entry)
       return {
         value = entry,
-        display = entry[1],
-        ordinal = entry[1],
-        path = entry[3],
-        lnum = entry[4],
+        display = entry.name,
+        ordinal = entry.name,
+        path = entry.filepath,
+        lnum = entry.line,
       }
     end,
   }
