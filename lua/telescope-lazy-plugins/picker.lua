3e301a3b886bc4db7ddde1faf3680d68e6683c25
polirritmico <ebray187@gmail.com>
2024-02-07 22:27:09 -0300

    refactor: Clean code of helper function

--- lua/telescope-lazy-plugins/picker.lua
+++ lua/telescope-lazy-plugins/picker.lua
@@ -14,9 +14,9 @@ local TelescopeLazyPicker = {}
 ---@field line integer Line number of the plugin definition in the lua file
 local LazyPluginSpecData = {}
 
----Finds the line number matching the plugin repository name within the plugin file and updates the 'line' field of the provided 'plugin' object accordingly.
+---Finds the line number matching the plugin repository name within the plugin file and updates the `line` field of the provided `plugin` object accordingly.
 ---@param plugin LazyPluginData
-local function _find_line_number(plugin)
+local function _search_and_set_the_line_number(plugin)
   local line = 1
   for line_str in io.lines(plugin.filepath) do
     if string.find(line_str, plugin.repo_name, 1, true) then
@@ -33,26 +33,28 @@ end
 ---repository name is found.
 ---@return table<LazyPluginData>
 local function _get_plugins_data()
-  local spec_files = {}
+  local plugins_collection = {}
   local lazy_plugins = require("lazy").plugins()
   local config_path = vim.fn.stdpath("config")
 
   for _, lazy_plugin in pairs(lazy_plugins) do
-    ---@type LazyPluginData
-    local plugin = { name = "", repo_name = "", filepath = "", line = 0 }
-    plugin.name = lazy_plugin.name
-    plugin.repo_name = lazy_plugin[1]
-    if plugin.name ~= "lazy.nvim" then
-      local module = lazy_plugin._.super == nil and lazy_plugin._.module
-        or lazy_plugin._.super._.module
-      module = module:gsub("%.", "/")
-      plugin.filepath = string.format("%s/lua/%s.lua", config_path, module)
+    if lazy_plugin.name ~= "lazy.nvim" then
+      local module_file = lazy_plugin._.super ~= nil and lazy_plugin._.super._.module
+        or lazy_plugin._.module
+      module_file = module_file:gsub("%.", "/")
 
-      _find_line_number(plugin)
-      table.insert(spec_files, plugin)
+      ---@type LazyPluginData
+      local plugin = {
+        name = lazy_plugin.name,
+        repo_name = lazy_plugin[1],
+        filepath = string.format("%s/lua/%s.lua", config_path, module_file),
+        line = 0,
+      }
+      _search_and_set_the_line_number(plugin)
+      table.insert(plugins_collection, plugin)
     end
   end
-  return spec_files
+  return plugins_collection
 end
 
 function TelescopeLazyPicker.finder()
