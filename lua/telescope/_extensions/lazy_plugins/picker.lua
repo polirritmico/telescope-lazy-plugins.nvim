4d52fcef2ddb3d63bca620b7cfb61b4d822fcee2
polirritmico <ebray187@gmail.com>
2024-02-24 04:05:16 -0300

    feat: pass opts to the picker to allow user customization

--- lua/telescope/_extensions/lazy_plugins/picker.lua
+++ lua/telescope/_extensions/lazy_plugins/picker.lua
@@ -4,15 +4,18 @@ local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 local action_set = require("telescope.actions.set")
 local lp_finder = require("telescope._extensions.lazy_plugins.finder")
+local lp_config = require("telescope._extensions.lazy_plugins.config")
 
 ---Custom picker action to open the file and move the current line at the top.
 ---@param prompt_bufnr integer Telescope prompt buffer value
 local function lp_action(prompt_bufnr)
   -- Append to Telescope history
-  action_state.get_current_history():append(
-    action_state.get_current_line(),
-    action_state.get_current_picker(prompt_bufnr)
-  )
+  action_state
+    .get_current_history()
+    :append(
+      action_state.get_current_line(),
+      action_state.get_current_picker(prompt_bufnr)
+    )
   -- Open the file in a new buffer
   action_set.select(prompt_bufnr, "default")
   -- Set current line at the top position of the view
@@ -22,7 +25,7 @@ end
 ---Telescope picker to quickly open plugins configuration files within the Lazy spec.
 ---@param opts table? Options passed to the Telescope previewer and sorter.
 local function lp_picker(opts)
-  opts = opts or {}
+  opts = vim.tbl_deep_extend("force", lp_config.options.picker_opts or {}, opts or {})
 
   pickers
     .new(opts, {
