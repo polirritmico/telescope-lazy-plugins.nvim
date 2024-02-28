3b93f64ad4db76fec004396d20d448953d017a82
polirritmico <ebray187@gmail.com>
2024-02-28 19:12:19 -0300

    refactor: Changes to `open_repo_url` action.

--- lua/telescope/_extensions/lazy_plugins/actions.lua
+++ lua/telescope/_extensions/lazy_plugins/actions.lua
@@ -22,40 +22,39 @@ end
 ---Dummy function to not close Telescope from mouse clicks.
 function lp_actions.nothing() end
 
----Custom picker action to open the repo url in a browser
+local open_url_cmd = "" ---@type string|nil
+
+---Custom picker action to open the repo url in the default browser
 function lp_actions.open_repo_url()
   local entry = action_state.get_selected_entry()
   local repo_url = entry.value.repo_url
-  if type(repo_url) ~= "string" or repo_url == "" then
+  if type(repo_url) ~= "string" or repo_url == "" or not open_url_cmd then
     return
   end
 
-  local cmd
-  if vim.fn.executable("xdg-open") == 1 then
-    cmd = "xdg-open"
-  elseif vim.fn.executable("open") == 1 then
-    cmd = "open"
-  elseif vim.fn.executable("start") == 1 then
-    cmd = "start"
-  elseif vim.fn.executable("wslview") == 1 then
-    cmd = "wslview"
-  else
-    vim.notify(
-      "Error: Missing supported url handler (xdg-open, open, start, wslview).",
-      vim.log.levels.ERROR,
-      { title = "Telescope Lazy Plugins" }
-    )
-    return
+  if open_url_cmd == "" then
+    if vim.fn.executable("xdg-open") == 1 then
+      open_url_cmd = "xdg-open"
+    elseif vim.fn.executable("open") == 1 then
+      open_url_cmd = "open"
+    elseif vim.fn.executable("start") == 1 then
+      open_url_cmd = "start"
+    elseif vim.fn.executable("wslview") == 1 then
+      open_url_cmd = "wslview"
+    else
+      open_url_cmd = nil
+      vim.notify(
+        "Error: Missing supported url handler (xdg-open, open, start or wslview).",
+        vim.log.levels.ERROR,
+        { title = "Telescope Lazy Plugins" }
+      )
+      return
+    end
   end
 
-  local cmd_output = vim.fn.jobstart({ cmd, repo_url }, { detach = true })
+  local cmd_output = vim.fn.jobstart({ open_url_cmd, repo_url }, { detach = true })
   if cmd_output <= 0 then
-    local msg = string.format(
-      "Error opening '%s' with '%s':\nreturn '%d'",
-      repo_url,
-      cmd,
-      cmd_output
-    )
+    local msg = string.format("Error opening '%s' with '%s'.", repo_url, open_url_cmd)
     vim.notify(msg, vim.log.levels.ERROR, { title = "Telescope Lazy Plugins" })
   end
 end
