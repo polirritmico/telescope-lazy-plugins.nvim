ae7386107db76c26a305a396773ad0775c158321
polirritmico <ebray187@gmail.com>
2024-07-08 18:57:10 -0400

    refactor: Move types from config

--- lua/telescope/_extensions/lazy_plugins/config.lua
+++ lua/telescope/_extensions/lazy_plugins/config.lua
@@ -1,23 +1,6 @@
 local lp_actions = require("telescope._extensions.lazy_plugins.actions")
 local lp_highlights = require("telescope._extensions.lazy_plugins.highlights")
 
----@class TelescopeLazyPluginsConfig
----@field lazy_config string? Optional. Path to the file containing the lazy opts and setup() call
----@field lazy_spec_table string? Optional. Path to the file containing the lazy plugin spec table
----@field mappings table Keymaps attached to the picker. See `:h telescope.mappings`
----@field name_only boolean Match only the `repo_name`, false to match the full `account/repo_name`
----@field picker_opts table Layout options passed into Telescope. Check `:h telescope.layout`
----@field show_disabled boolean Also show disabled plugins from the Lazy spec
----@field custom_entries? table<LazyPluginsCustomEntry|LazyPluginData> Table to pass custom entries to the picker.
----@field live_grep? table Options to pass into the `live_grep` telescope builtin picker
-
----@class LazyPluginsCustomEntry
----@field name string Entry name
----@field filepath string Full file path to the lua target file
----@field line? integer Optional: Line number to set the view on the target file
----@field repo_url? string Optional: Url to open with the `open_repo_url` action
----@field repo_dir? string Optional: Directory path to open with the `open_repo_dir` action
-
 local M = {}
 
 ---@type TelescopeLazyPluginsConfig
--- lua/telescope/_extensions/lazy_plugins/types.lua
+++ lua/telescope/_extensions/lazy_plugins/types.lua
@@ -20,3 +20,19 @@
 ---@class LazyPluginFragment
 ---@field mod LazyMinSpec
 ---@field path string
+
+---@class TelescopeLazyPluginsConfig
+---@field lazy_config string? Optional. Path to the file containing the lazy opts and setup() call
+---@field mappings table Keymaps attached to the picker. See `:h telescope.mappings`
+---@field name_only boolean Match only the `repo_name`, false to match the full `account/repo_name`
+---@field picker_opts table Layout options passed into Telescope. Check `:h telescope.layout`
+---@field show_disabled boolean Also show disabled plugins from the Lazy spec
+---@field custom_entries? table<LazyPluginsCustomEntry|LazyPluginData> Table to pass custom entries to the picker.
+---@field live_grep? table Options to pass into the `live_grep` telescope builtin picker
+
+---@class LazyPluginsCustomEntry
+---@field name string Entry name
+---@field filepath string Full file path to the lua target file
+---@field line? integer Optional: Line number to set the view on the target file
+---@field repo_url? string Optional: Url to open with the `open_repo_url` action
+---@field repo_dir? string Optional: Directory path to open with the `open_repo_dir` action
