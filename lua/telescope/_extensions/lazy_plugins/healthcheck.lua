0ea36bff618e42419ea4b7aa539410c833631b58
polirritmico <ebray187@gmail.com>
2024-07-10 14:01:35 -0400

    feat(healthcheck): Add checks for old lazy versions

--- lua/telescope/_extensions/lazy_plugins/healthcheck.lua
+++ lua/telescope/_extensions/lazy_plugins/healthcheck.lua
@@ -34,41 +34,60 @@ local function check_health()
     ok("Lazy configuration file was found.")
   end
 
-  --- Check plugins imports and search lines
-  local finder = telescope.extensions.lazy_plugins.finder
-  local collection_ok, plugins_collection = pcall(finder.finder)
-  if not collection_ok then
-    warn("Problems detected importing plugin configurations. Maybe missing entries.")
-  else
-    ok("No problems importing plugins config specs.")
-  end
-  local min_plugins = 4 -- at least: lazy, telescope, plenary and telescope-lazy-plugins
-  if #plugins_collection.results < min_plugins then
-    error("Missing plugins (at least 4). Check configuration.")
+  local lazy_version = tonumber(require("lazy.core.config").version:sub(1, 2))
+
+  if lazy_version < 11 then
+    if not opts.lazy_config then
+      local min_plugins = (vim.uv or vim.loop).fs_stat(opts.lazy_config) and 2 or 1
+      local lazy_plugins = telescope.extensions.lazy_plugins.finder.finder().results
+      if #lazy_plugins < min_plugins then
+        error("No plugins configuration files found. Check the `lazy_config` path.")
+      else
+        warn("No Lazy plugins spec table file found. (Set in `lazy_config`)")
+      end
+    else
+      ok("Path to Lazy plugins spec table file found.")
+    end
+    return
   end
 
-  local plugins_without_matches = {}
-  for _, plugin in pairs(plugins_collection.results) do
-    local full_name = plugin.value.full_name
-    if full_name ~= "folke/lazy.nvim" then
-      local filepath = plugin.value.filepath
-      if plugin.value.line == 1 then -- since 1 is the default value only check those plugins
-        local _, match = finder.line_number_search(full_name, filepath)
-        if not match then
-          table.insert(plugins_without_matches, { name = full_name, path = filepath })
+  if lazy_version >= 11 then
+    --- Check plugins imports and search lines
+    local finder = telescope.extensions.lazy_plugins.finder
+    local collection_ok, plugins_collection = pcall(finder.finder)
+    if not collection_ok then
+      warn("Problems detected importing plugin configurations. Maybe missing entries.")
+    else
+      ok("No problems importing plugins config specs.")
+    end
+    local min_plugins = 4 -- at least: lazy, telescope, plenary and telescope-lazy-plugins
+    if #plugins_collection.results < min_plugins then
+      error("Missing plugins (at least 4). Check configuration.")
+    end
+
+    local plugins_without_matches = {}
+    for _, plugin in pairs(plugins_collection.results) do
+      local full_name = plugin.value.full_name
+      if full_name ~= "folke/lazy.nvim" then
+        local filepath = plugin.value.filepath
+        if plugin.value.line == 1 then -- since 1 is the default value only check those plugins
+          local _, match = finder.line_number_search(full_name, filepath)
+          if not match then
+            table.insert(plugins_without_matches, { name = full_name, path = filepath })
+          end
         end
       end
     end
-  end
-  if #plugins_without_matches > 0 then
-    local msg = "Problems detected searching plugin(s) in the config files:\n"
-    for _, plugin in pairs(plugins_without_matches) do
-      msg = msg .. "- name: '" .. plugin.name .. "'\n"
-      msg = msg .. "  file: '" .. plugin.path .. "'\n"
+    if #plugins_without_matches > 0 then
+      local msg = "Problems detected searching plugin(s) in the config files:\n"
+      for _, plugin in pairs(plugins_without_matches) do
+        msg = msg .. "- name: '" .. plugin.name .. "'\n"
+        msg = msg .. "  file: '" .. plugin.path .. "'\n"
+      end
+      error(msg)
+    else
+      ok("Found all imported plugin configurations in the module files.")
     end
-    error(msg)
-  else
-    ok("Found all imported plugin configurations in the module files.")
   end
 
   --- Check custom user entries
