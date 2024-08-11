9a73e47552c49ec3386e2dc99cf97bba49f533c8
polirritmico <ebray187@gmail.com>
2024-08-11 03:07:35 -0400

    feat!: Change default keymaps `<C-g>r` and `<C-g>d`
    Updated key mappings for better mnemonic association:
    - <C-g>r now opens the selected plugin [r]eadme
    - <C-g>d now opens the selected plugin [d]irectory

--- lua/telescope/_extensions/lazy_plugins/config.lua
+++ lua/telescope/_extensions/lazy_plugins/config.lua
@@ -13,17 +13,17 @@ local defaults = {
   ignore_imports = {}, -- Add imports you want to ignore, e.g., "lazyvim.plugins".
   mappings = {
     ["i"] = {
-      ["<C-g>d"] = lp_actions.open_readme,
-      ["<C-g>r"] = lp_actions.open_repo_dir,
+      ["<C-g>d"] = lp_actions.open_repo_dir,
       ["<C-g>f"] = lp_actions.open_repo_find_files,
       ["<C-g>l"] = lp_actions.open_repo_live_grep,
+      ["<C-g>r"] = lp_actions.open_readme,
       ["<C-g>x"] = lp_actions.open_repo_url,
     },
     ["n"] = {
-      ["gd"] = lp_actions.open_readme,
-      ["gr"] = lp_actions.open_repo_dir,
+      ["gd"] = lp_actions.open_repo_dir,
       ["gf"] = lp_actions.open_repo_find_files,
       ["gl"] = lp_actions.open_repo_live_grep,
+      ["gr"] = lp_actions.open_readme,
       ["gx"] = lp_actions.open_repo_url,
     },
   },
