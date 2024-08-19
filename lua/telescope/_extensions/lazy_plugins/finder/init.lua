e9677d9bafe92e62927538b0d7541814d938f4ba
polirritmico <ebray187@gmail.com>
2024-08-19 09:37:22 -0400

    feat!: Remove support for lazy < v11.0

--- lua/telescope/_extensions/lazy_plugins/finder/init.lua
+++
@@ -1,7 +0,0 @@
-local lazy_version = tonumber(require("lazy.core.config").version:sub(1, 2))
-
-if lazy_version < 11 then
-  return require("telescope._extensions.lazy_plugins.finder.finder_old")
-else
-  return require("telescope._extensions.lazy_plugins.finder.finder")
-end
--- lua/telescope/_extensions/lazy_plugins/healthcheck.lua
+++ lua/telescope/_extensions/lazy_plugins/healthcheck.lua
@@ -56,78 +56,59 @@ local function check_health()
     ok(string.format("lazy_config found: `%s`", opts.lazy_config))
   end
 
-  --- Check entries
+  --- Entries
 
-  local lazy_version = tonumber(require("lazy.core.config").version:sub(1, 2))
-
-  if lazy_version < 11 then
-    if not opts.lazy_config then
-      local min_plugins = (vim.uv or vim.loop).fs_stat(opts.lazy_config) and 2 or 1
-      local lazy_plugins = telescope.extensions.lazy_plugins.finder.finder().results
-      if #lazy_plugins < min_plugins then
-        error("No plugins configuration files found. Check the `lazy_config` path.")
-      else
-        warn("No Lazy plugins spec table file found. (Set in `lazy_config`)")
-      end
-    else
-      ok("Path to Lazy plugins spec table file found.")
-    end
-    return
+  --- Check plugins imports and search lines
+  local finder = telescope.extensions.lazy_plugins.finder
+  local collection_ok, plugins_collection = pcall(finder.finder)
+  if not collection_ok then
+    warn("Problems detected importing plugin configurations. Maybe missing entries.")
+  else
+    ok("No problems importing plugins config specs.")
+  end
+  local min_plugins = 4 -- at least: lazy, telescope, plenary and telescope-lazy-plugins
+  if #plugins_collection.results < min_plugins then
+    error("Missing plugins (at least 4). Check configuration.")
   end
 
-  if lazy_version >= 11 then
-    --- Check plugins imports and search lines
-    local finder = telescope.extensions.lazy_plugins.finder
-    local collection_ok, plugins_collection = pcall(finder.finder)
-    if not collection_ok then
-      warn("Problems detected importing plugin configurations. Maybe missing entries.")
-    else
-      ok("No problems importing plugins config specs.")
-    end
-    local min_plugins = 4 -- at least: lazy, telescope, plenary and telescope-lazy-plugins
-    if #plugins_collection.results < min_plugins then
-      error("Missing plugins (at least 4). Check configuration.")
-    end
-
-    ---@param entry LazyPluginsData
-    ---@return boolean
-    local function is_custom_entry(entry)
-      local custom_entries = vim.tbl_get(config, "options", "custom_entries")
-      if custom_entries and #custom_entries > 0 then
-        for _, custom in pairs(custom_entries) do
-          if entry.name == custom.name and entry.filepath == custom.filepath then
-            return true
-          end
-        end
-      end
-      return false
-    end
-
-    local plugins_without_matches = {}
-    for _, plugin in pairs(plugins_collection.results) do
-      local full_name = plugin.value.full_name
-      if not is_custom_entry(plugin.value) and full_name ~= "folke/lazy.nvim" then
-        local filepath = plugin.value.filepath
-        -- Only check line 1 plugins since that's the default
-        if plugin.value.line == 1 then
-          local _, match = finder.line_number_search(full_name, filepath)
-          if not match then
-            table.insert(plugins_without_matches, { name = full_name, path = filepath })
-          end
+  ---@param entry LazyPluginsData
+  ---@return boolean
+  local function is_custom_entry(entry)
+    local custom_entries = vim.tbl_get(config, "options", "custom_entries")
+    if custom_entries and #custom_entries > 0 then
+      for _, custom in pairs(custom_entries) do
+        if entry.name == custom.name and entry.filepath == custom.filepath then
+          return true
         end
       end
     end
+    return false
+  end
 
-    if #plugins_without_matches > 0 then
-      local msg = "Problems detected searching plugin(s) in the config files:\n"
-      for _, plugin in pairs(plugins_without_matches) do
-        msg = msg .. "- name: '" .. plugin.name .. "'\n"
-        msg = msg .. "  file: '" .. plugin.path .. "'\n"
+  local plugins_without_matches = {}
+  for _, plugin in pairs(plugins_collection.results) do
+    local full_name = plugin.value.full_name
+    if not is_custom_entry(plugin.value) and full_name ~= "folke/lazy.nvim" then
+      local filepath = plugin.value.filepath
+      -- Only check line 1 plugins since that's the default
+      if plugin.value.line == 1 then
+        local _, match = finder.line_number_search(full_name, filepath)
+        if not match then
+          table.insert(plugins_without_matches, { name = full_name, path = filepath })
+        end
       end
-      error(msg)
-    else
-      ok("Found all imported plugin configurations in the module files.")
     end
+  end
+
+  if #plugins_without_matches > 0 then
+    local msg = "Problems detected searching plugin(s) in the config files:\n"
+    for _, plugin in pairs(plugins_without_matches) do
+      msg = msg .. "- name: '" .. plugin.name .. "'\n"
+      msg = msg .. "  file: '" .. plugin.path .. "'\n"
+    end
+    error(msg)
+  else
+    ok("Found all imported plugin configurations in the module files.")
   end
 
   --- Check custom user entries
